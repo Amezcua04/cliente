5c09552877f04c3841192be1800c940987adae30
CarlosAmezcua <Amezcua041196@gmail.com>
2024-06-05 20:46:26 -0700

    restauracion

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import { useEffect } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Home from "./scenes/home/Home"
-import Navbar from "./scenes/global/Navbar";
-import Details from "./scenes/details/Details";
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
-  <div className="app">
-    <BrowserRouter>
-    <ScrollToTop />
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/vehiculos/:url" element={<Details />} />
-    </Routes>
-    </BrowserRouter>
-  </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
